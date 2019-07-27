9f34694c12c066eac68a1bfa59924e677ef88689
MaxRobertsDear <maxhroberts@gmail.com>
2019-07-27 18:29:58 +0100

    Added prompt to add ingredients When ingredients.length === 0, prompt user to add ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,12 +5,18 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
   // incoming props (ingredients) are an object. Therefore need to transform into array before mapping over
-  const transformedIngredients = Object.keys(props.ingredients) // Object.keys extracts the keys of an object and puts them into an array
+  let transformedIngredients = Object.keys(props.ingredients) // Object.keys extracts the keys of an object and puts them into an array
     .map(igKey => { 
       return [...Array(props.ingredients[igKey])].map((_, i) => {
         return <BurgerIngredient key={igKey + 1} type={igKey} />
       })
     })
+    .reduce((arr, el) => {
+      return arr.concat(el)
+    }, [])
+    if (transformedIngredients.length === 0){
+      transformedIngredients = <p>Please start adding ingredients</p>
+    }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
