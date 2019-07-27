507a1605f6fe654d53fa3e3a5b535adc41caad6d
MaxRobertsDear <maxhroberts@gmail.com>
2019-07-27 18:17:26 +0100

    Added dynamic output of ingredients Added state to <BurgerBuilder /> and passed props to <Burger />

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import classes from './Burger.module.css'
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
+
+const burger = (props) => {
+  // incoming props (ingredients) are an object. Therefore need to transform into array before mapping over
+  const transformedIngredients = Object.keys(props.ingredients) // Object.keys extracts the keys of an object and puts them into an array
+    .map(igKey => { 
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + 1} type={igKey} />
+      })
+    })
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  )
+}
+
+export default burger
