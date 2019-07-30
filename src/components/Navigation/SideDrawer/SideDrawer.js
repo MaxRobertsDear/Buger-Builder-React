5c551c4897f7e1b0bcde5ec2176bae081aa3e073
MaxRobertsDear <maxhroberts@gmail.com>
2019-07-30 11:42:40 +0100

    <SideDrawer /> disappears when <Backdrop /> clicked

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -7,11 +7,16 @@ import Aux from '../../../hoc/Aux'
 import NavigationItems from '../../Navigation/NavigationItems/NavigationItems'
 
 const sideDrawer = (props) => {
-
+  let attachedClasses = [classes.SideDrawer, classes.Close]
+  if (props.open) {
+    attachedClasses = [classes.SideDrawer, classes.Open]
+  }
+  console.log(attachedClasses)
+  console.log(attachedClasses.join(' '))
   return (
     <Aux>
       <Backdrop show={props.open} clicked={props.closed} />
-      <div className={classes.SideDrawer}>
+      <div className={attachedClasses.join(' ')}>
         <div className={classes.Logo}>
           <Logo />
         </div>
