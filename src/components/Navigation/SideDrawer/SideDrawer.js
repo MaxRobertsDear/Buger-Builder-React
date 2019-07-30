a2ee997db10feed11eb60b55380e0f6466a24d11
MaxRobertsDear <maxhroberts@gmail.com>
2019-07-30 11:29:02 +0100

    Added <Backdrop /> to <SideDrawer /> Backdrop appears when <SideDrawer /> is active. Need to add click listener so that user can exit SideDrawer and menu buttons for navigating in/out of SideDrawer

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,19 +2,24 @@ import React from 'react'
 
 import classes from './SideDrawer.module.css'
 import Logo from '../../Logo/Logo'
+import Backdrop from '../../UI/Backdrop/Backdrop'
+import Aux from '../../../hoc/Aux'
 import NavigationItems from '../../Navigation/NavigationItems/NavigationItems'
 
 const sideDrawer = (props) => {
 
   return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo />
+    <Aux>
+      <Backdrop show />
+      <div className={classes.SideDrawer}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Aux>
   )
 }
 
