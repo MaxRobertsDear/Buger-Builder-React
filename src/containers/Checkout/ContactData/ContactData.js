6161fed57ea863fce5432e4ad98421ecc2a2aee1
MaxRobertsDear <maxhroberts@gmail.com>
2019-07-31 13:07:18 +0100

    Added route to '/' from <ContactData /> User will be re-directed to homepage upon submitting the order.

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import Button from '../../../components/UI/Button/Button'
 import classes from './ContactData.module.css'
 import axios from '../../../axios-orders'
+import Spinner from '../../../components/UI/Spinner/Spinner'
 
 class ContactData extends Component {
   state = {
@@ -35,6 +36,7 @@ class ContactData extends Component {
     axios.post('/orders.json', order)
     .then( reponse => {
       this.setState({loading: false})
+      this.props.history.push('/')
     })
     .catch(error => {
       this.setState({loading: false})
@@ -42,16 +44,22 @@ class ContactData extends Component {
   }
 
   render () {
+    let form = (
+      <form>
+      <input type='text' name='name' placeholder='Your name' />
+      <input type='email' name='email' placeholder='Your email' />
+      <input type='text' name='street' placeholder='Your street' />
+      <input type='text' name='postal' placeholder='Your post code' />
+      <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+    </form>
+    ) 
+    if (this.state.loadng) {
+      form = <Spinner />
+    }
     return (
       <div className={classes.ContactData}>
         <h4>Enter your Contact Info</h4>
-        <form>
-          <input type='text' name='name' placeholder='Your name' />
-          <input type='email' name='email' placeholder='Your email' />
-          <input type='text' name='street' placeholder='Your street' />
-          <input type='text' name='postal' placeholder='Your post code' />
-          <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
-        </form>
+        {form}
       </div>
     )
   }
