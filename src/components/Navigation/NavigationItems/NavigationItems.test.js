61b0c7e90eca1e97274858e176c13f05b6bd14cd
MaxRobertsDear <maxhroberts@gmail.com>
2019-07-31 17:12:46 +0100

    Added test using ShallowRenderer Wrote the same test to compare Jest ShallowRenderer to enzyme shallow

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -6,12 +6,31 @@ import Adapter from 'enzyme-adapter-react-16'
 import NavigationItems from './NavigationItems'
 import NavigationItem from './NavigationItem/NavigationItem'
 
+import ShallowRenderer from 'react-test-renderer/shallow'
+
 
 configure({adapter: new Adapter()})
 
-describe('<NavigationItems />', () => {
-  it('should render two <Navigation /> elements if not authenticated', () => {
-    const wrapper = shallow(<NavigationItems />)
-    expect(wrapper.find(NavigationItem)).toHaveLength(2)
+describe('Using enzyme shallow', () => {
+  describe('<NavigationItems />', () => {
+    it('should render two <Navigation /> elements if not authenticated', () => {
+      const wrapper = shallow(<NavigationItems />)
+      expect(wrapper.find(NavigationItem)).toHaveLength(2)
+    })
+  })
+})
+
+describe('Using Jest ShowllowRenderer', () => {
+  describe('<NavigationItems />', () => {
+    it('should render two <Navigation /> elements if not authenticated', () => {
+      const renderer = new ShallowRenderer()
+      renderer.render(<NavigationItems />)
+      const result = renderer.getRenderOutput()
+      
+      expect(result.props.children).toEqual([
+        <NavigationItem link='/'>BurgerBuilder</NavigationItem>,
+        <NavigationItem link='/orders'>Orders</NavigationItem>
+      ]);
+    })
   })
 })
