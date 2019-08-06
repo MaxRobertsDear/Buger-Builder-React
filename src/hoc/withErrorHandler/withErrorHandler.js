acb0b5187f54f6d017209307875bb9d5736b1207
MaxRobertsDear <maxhroberts@gmail.com>
2019-08-06 16:58:27 +0100

    Refactor withErrorHandler Converted from class-based component to functional component, using useState() for manipulating state and useEffect() for componentWillUnmount - ensuring to pass a second argument so that the program knows when to perform the cleanup

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,51 +1,49 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Modal from '../../components/UI/Modal/Modal'
 
 // takes axios as second argument so that the wrapper knows whether there is an error to show
 const withErrorHandler = (WrappedComponent, axios) => {
-  return class extends Component {
-    state = {
-      error: null
-    }
-    componentWillMount () {
-      this.reqInterceptor = axios.interceptors.request.use(req => {
-        // clear any errors when sending a request
-        this.setState({error: null})
-        return req
-      })
-      this.resInterceptor = axios.interceptors.response.use(res => res, error => {
-        // set error state to the error returned by firebase 
-        this.setState({error: error})
-      })
-    }
+  return (props) => {
+    const [error, setError] = useState(null)
 
-    componentWillUnmount () {
-      axios.interceptors.request.eject(this.reqInterceptor)
-      axios.interceptors.response.eject(this.resInterceptor)
-    }
+    const reqInterceptor = axios.interceptors.request.use(req => {
+      // clear any errors when sending a request
+      setError(null)
+      return req
+    })
+    const resInterceptor = axios.interceptors.response.use(res => res, err => {
+      // set error state to the error returned by firebase 
+      setError(err)
+    })
 
-    errorConfirmedHandler=() => {
-      this.setState({error: null})
-    }
 
-    render () {
-      return (
-        <>
-          <Modal 
-            show={this.state.error}
-            // remove error message when backdrop is clicked
-            // ternary expression because Modal is always present (even if not shown)
-            modalClosed={this.errorConfirmedHandler}>
-            {this.state.error ? this.state.error.message : null}
-          </Modal>
-          <WrappedComponent {...this.props} />
-        </>
-          // distribute any props that might be received with the wrapped component because I don't know these props
-          // but definitely want to show them
-      )
-    }
+    useEffect(() => {
+      return () => {
+        axios.interceptors.request.eject(reqInterceptor)
+        axios.interceptors.response.eject(resInterceptor)
+      }
+    }, [reqInterceptor, resInterceptor])
+  
+    const errorConfirmedHandler=() => {
+      setError(null)
+  }
+
+    return (
+      <>
+        <Modal 
+          show={error}
+          // remove error message when backdrop is clicked
+          // ternary expression because Modal is always present (even if not shown)
+          modalClosed={errorConfirmedHandler}>
+          {error ? error.message : null}
+        </Modal>
+        <WrappedComponent {...props} />
+      </>
+        // distribute any props that might be received with the wrapped component because I don't know these props
+        // but definitely want to show them
+    )
   }
 }
 
