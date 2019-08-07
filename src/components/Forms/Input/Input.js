1d623673b955a7325b5c17f92036a25395961641
MaxRobertsDear <maxhroberts@gmail.com>
2019-08-07 21:46:46 +0100

    Added key to 'select' map()

--- src/components/Forms/Input/Input.js
+++ src/components/Forms/Input/Input.js
@@ -25,7 +25,7 @@ const input = (props) => {
           className={classes.InputElement} 
           value={props.value}>
             {props.elementConfig.options.map(option => (
-              <option value={option.value}>
+              <option key={option.value} value={option.value}>
                 {option.displayValue}
               </option>
             ))}
